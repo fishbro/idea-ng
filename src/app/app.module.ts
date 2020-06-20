becfb5cb7274a9b36e5c2b063b245bd243eb19d3
[fish_bro] <[info@fish-bro.pro]>
2020-06-20 23:50:08 +0300

    add "to top" button, mobile main menu markups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GallerySectionsComponent } from './gallery-sections/gallery-sections.component';
 import { MarkupsPageComponent } from './markups-page/markups-page.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { ToTopBtnComponent } from './to-top-btn/to-top-btn.component';
+import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GallerySectionsComponent,
-    MarkupsPageComponent
+    MarkupsPageComponent,
+    ToTopBtnComponent,
+    MobileMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+		BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/to-top-btn/to-top-btn.component.ts
@@ -0,0 +1,58 @@
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
+
+@Component({
+  selector: 'app-to-top-btn',
+  templateUrl: './to-top-btn.component.html',
+  styleUrls: ['./to-top-btn.component.scss'],
+	animations: [
+		trigger('inOutAnimation', [
+			transition(':enter', [
+				style({opacity: 0}),
+				animate('.3s', style({
+					opacity: 1
+				}))
+			]),
+			transition(':leave', [
+				style({opacity: 1}),
+				animate('.3s', style({
+					opacity: 0
+				}))
+			])
+		])
+	]
+})
+export class ToTopBtnComponent implements OnInit{
+	@ViewChild('toTop', {static: true}) buttonRef: ElementRef
+	windowHeight: number = window.innerHeight
+	isScrollToTop: boolean = false
+	isStuck: boolean = false
+	parentNode: HTMLElement
+
+	ngOnInit(): void {
+		this.parentNode = this.buttonRef.nativeElement
+
+		this.isScrollToTop = this.checkScroll()
+		this.isStuck = this.checkIsStuck(this.parentNode)
+	}
+	checkScroll(): boolean {
+		return (window.pageYOffset > this.windowHeight/2)
+	}
+	checkIsStuck(parent: HTMLElement): boolean {
+		return ((window.pageYOffset+this.windowHeight) > (this.parentNode.offsetTop+37))
+	}
+	@HostListener('window:scroll', ['$event'])
+	onScroll(e): void {
+		this.isScrollToTop = this.checkScroll()
+		this.isStuck = this.checkIsStuck(this.parentNode)
+	}
+	scrollToTop(): void {
+		(function smoothscroll(): void {
+			let currentScroll: number = window.pageYOffset
+			if (currentScroll > 0) {
+				window.requestAnimationFrame(smoothscroll)
+				window.scrollTo(0, currentScroll - (currentScroll / 8))
+			}
+		})()
+	}
+}
